f20f2335a3aca6902a3a3eae3031ed9945ca9de4
ChromaKey81 <williamcipher2006@gmail.com>
2021-06-30 11:07:35 -0400

    first commit

---
+++ src/main/java/net/devsdream/Main.java
@@ -0,0 +1,16 @@
+package net.devsdream;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        final Item FABRIC_ITEM = new Item(new Item.Settings().maxDamageIfAbsent(3));
+        Registry.register(Registry.ITEM, new Identifier("tutorial", "fabric_item"), FABRIC_ITEM);
+    }
+    
+}
