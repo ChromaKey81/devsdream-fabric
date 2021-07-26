459f97ed49680e6874898430c30a50bfd26d487f
ChromaKey81 <williamcipher2006@gmail.com>
2021-07-25 22:43:56 -0400

    fixed shaped nbt crafting

--- src/main/java/net/devsdream/crafting/ShapedNBTRecipe.java
+++ src/main/java/net/devsdream/crafting/ShapedNBTRecipe.java
@@ -35,14 +35,14 @@ public class ShapedNBTRecipe extends ShapedRecipe {
 
     public static class Serializer implements RecipeSerializer<ShapedNBTRecipe> {
         public ShapedNBTRecipe read(Identifier identifier, JsonObject jsonObject) {
-           String string = JsonHelper.getString(jsonObject, "group", "");
-           Map<String, Ingredient> map = ShapedRecipe.readSymbols(JsonHelper.getObject(jsonObject, "key"));
-           String[] strings = ShapedRecipe.removePadding(ShapedRecipe.getPattern(JsonHelper.getArray(jsonObject, "pattern")));
-           int i = strings[0].length();
-           int j = strings.length;
-           DefaultedList<Ingredient> defaultedList = ShapedRecipe.createPatternMatrix(strings, map, i, j);
-           ItemStack itemStack = ShapedRecipe.outputFromJson(JsonHelper.getObject(jsonObject, "result"));
-           return new ShapedNBTRecipe(identifier, string, i, j, defaultedList, itemStack);
+         String string = JsonHelper.getString(jsonObject, "group", "");
+         Map<String, Ingredient> map = ShapedRecipe.readSymbols(JsonHelper.getObject(jsonObject, "key"));
+         String[] strings = ShapedRecipe.removePadding(ShapedRecipe.getPattern(JsonHelper.getArray(jsonObject, "pattern")));
+         int i = strings[0].length();
+         int j = strings.length;
+         DefaultedList<Ingredient> defaultedList = ShapedRecipe.createPatternMatrix(strings, map, i, j);
+         ItemStack itemStack = ShapedNBTRecipe.outputFromJson(JsonHelper.getObject(jsonObject, "result"));
+         return new ShapedNBTRecipe(identifier, string, i, j, defaultedList, itemStack);
         }
   
         public ShapedNBTRecipe read(Identifier identifier, PacketByteBuf packetByteBuf) {
@@ -50,25 +50,26 @@ public class ShapedNBTRecipe extends ShapedRecipe {
            int j = packetByteBuf.readVarInt();
            String string = packetByteBuf.readString();
            DefaultedList<Ingredient> defaultedList = DefaultedList.ofSize(i * j, Ingredient.EMPTY);
-           for(int k = 0; k < defaultedList.size(); ++k) {
+
+           for (int k = 0; k < defaultedList.size(); ++k) {
               defaultedList.set(k, Ingredient.fromPacket(packetByteBuf));
            }
-  
+
            ItemStack itemStack = packetByteBuf.readItemStack();
            return new ShapedNBTRecipe(identifier, string, i, j, defaultedList, itemStack);
         }
-  
+
         public void write(PacketByteBuf packetByteBuf, ShapedNBTRecipe shapedRecipe) {
            packetByteBuf.writeVarInt(shapedRecipe.getWidth());
            packetByteBuf.writeVarInt(shapedRecipe.getHeight());
            packetByteBuf.writeString(shapedRecipe.getGroup());
-           Iterator<Ingredient> ingredients = shapedRecipe.getIngredients().iterator();
-  
-           while(ingredients.hasNext()) {
-              Ingredient ingredient = ingredients.next();
+           Iterator var3 = shapedRecipe.getIngredients().iterator();
+
+           while (var3.hasNext()) {
+              Ingredient ingredient = (Ingredient) var3.next();
               ingredient.write(packetByteBuf);
            }
-  
+
            packetByteBuf.writeItemStack(shapedRecipe.getOutput());
         }
      }
