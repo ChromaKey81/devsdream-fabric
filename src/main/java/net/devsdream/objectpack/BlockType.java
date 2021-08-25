461066b2da06874b5875c4b9e0161d1786605aea
ChromaKey81 <williamcipher2006@gmail.com>
2021-08-25 11:51:19 -0400

    let the ultra-customized blocks begin!

--- src/main/java/net/devsdream/objectpack/BlockType.java
+++ src/main/java/net/devsdream/objectpack/BlockType.java
@@ -1,13 +1,89 @@
 package net.devsdream.objectpack;
 
-import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+
+import net.devsdream.util.ChromaJsonHelper;
+import net.minecraft.block.BlockState;
+import net.minecraft.predicate.StatePredicate;
+import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.server.function.CommandFunction;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.JsonHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 
 public class BlockType extends ObjectType {
 
-    public BlockType(Identifier identifier, JsonObject object) {
+    protected StatePredicate hasRandomTicks = null;
+    protected Object propogatesSkylightDown = null;
+    protected Identifier animateTick = null;
+    protected Identifier onDestroy = null;
+    protected Identifier onDestroyExplosion = null;
+    protected Identifier onStep = null;
+    protected Identifier onPlayerBreak = null;
+    protected final Map<BlockState, LocationPredicate> placementStateMap = new HashMap<BlockState, LocationPredicate>();
+
+    public BlockType(Identifier identifier, JsonObject object) throws JsonSyntaxException {
         super(identifier, object);
+        if (object.get("randomly_ticking") != null) this.hasRandomTicks = StatePredicate.fromJson(object.get("randomly_ticking"));
+        if (object.get("propogates_skylight_down") != null) {
+            JsonElement element = object.get("propogates_skylight_down");
+            if (element.isJsonObject()) {
+                this.propogatesSkylightDown = StatePredicate.fromJson(object);
+            } else {
+                this.propogatesSkylightDown = JsonHelper.asBoolean(object, "boolean or block state predicate");
+            }
+        }
+        this.animateTick = new Identifier(ChromaJsonHelper.getStringOrDefault(object, "animate_tick", null));
+        this.onDestroy = new Identifier(ChromaJsonHelper.getStringOrDefault(object, "on_destroyed", null));
+        this.onDestroyExplosion = new Identifier(ChromaJsonHelper.getStringOrDefault(object, "on_exploded", null));
+        this.onStep = new Identifier(ChromaJsonHelper.getStringOrDefault(object, "on_step", null));
+    }
+
+    static CommandFunction getFunction(World world, Identifier identifier) {
+        return world.getServer().getCommandFunctionManager().getFunction(identifier).orElseGet(() -> null);
+    }
+
+    static CommandFunction getFunctionWorldAccess(WorldAccess world, Identifier identifier) {
+        return world.getServer().getCommandFunctionManager().getFunction(identifier).orElseGet(() -> null);
+    }
+
+    public StatePredicate getRandomTickPredicate() {
+        return this.hasRandomTicks;
+    }
+
+    public Object getPropogatesSkylightDown(BlockState state) {
+        if (this.propogatesSkylightDown != null) {
+            if (this.propogatesSkylightDown instanceof StatePredicate) {
+                return ((StatePredicate)this.propogatesSkylightDown).test(state);
+            } else {
+                return (boolean)this.propogatesSkylightDown;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public CommandFunction getAnimateTickFunction(World world) {
+        return getFunction(world, this.animateTick);
+    }
+
+    public CommandFunction getOnDestroyFunction(WorldAccess world) {
+        return getFunctionWorldAccess(world, this.onDestroy);
+    }
+
+    public CommandFunction getOnDestroyedByExplosionFunction(World world) {
+        return getFunction(world, this.onDestroyExplosion);
+    }
+
+    public CommandFunction getStepOnFunction(World world) {
+        return getFunction(world, this.onStep);
     }
     
 }
---
+++ src/main/java/net/devsdream/objectpack/customized/block/CustomizedBlock.java
@@ -0,0 +1,108 @@
+package net.devsdream.objectpack.customized.block;
+
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.devsdream.objectpack.BlockType;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.function.CommandFunction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.explosion.Explosion;
+
+public class CustomizedBlock extends Block {
+
+    private final BlockType type;
+
+    public CustomizedBlock(Settings settings, BlockType type) {
+        super(settings);
+        this.type = type;
+    }
+
+    static void executeFunction(Function<World, CommandFunction> function, World world, Function<World, ServerCommandSource> source, Consumer<?> elseDo) {
+        if (!world.isClient && function != null) {
+            world.getServer().getCommandFunctionManager().execute(function.apply(world), source.apply(world));
+        } else {
+            elseDo.accept(null);
+        }
+    }
+
+    static void executeFunctionWorldAccess(Function<WorldAccess, CommandFunction> function, WorldAccess world, Function<WorldAccess, ServerCommandSource> source, Consumer<?> elseDo) {
+        if (function != null) {
+            world.getServer().getCommandFunctionManager().execute(function.apply(world), source.apply(world));
+        } else {
+            elseDo.accept(null);
+        }
+    }
+
+    @Override
+    public boolean hasRandomTicks(BlockState state) {
+        if (this.type.getRandomTickPredicate() != null) {
+            return this.type.getRandomTickPredicate().test(state);
+        } else {
+            return super.hasRandomTicks(state);
+        }
+    }
+    
+    @Override
+    public boolean isTranslucent(BlockState state, BlockView world, BlockPos pos) {
+        if (this.type.getPropogatesSkylightDown(state) != null) {
+            return (boolean)this.type.getPropogatesSkylightDown(state);
+        } else {
+            return super.isTranslucent(state, world, pos);
+        }
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        executeFunction((w) -> this.type.getAnimateTickFunction(w), world, (w) -> w.getServer().getCommandSource().withPosition(Vec3d.of(pos)), (w) -> {
+            super.randomDisplayTick(state, world, pos, random);
+        });
+    }
+
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+        executeFunctionWorldAccess((w) -> this.type.getOnDestroyFunction(w), world, (w) -> w.getServer().getCommandSource().withPosition(Vec3d.of(pos)), (w) -> {
+            super.onBroken(world, pos, state);
+        });
+    }
+
+    @Override
+    public void onDestroyedByExplosion(World world, BlockPos pos, Explosion explosion) {
+        executeFunction((w) -> this.type.getOnDestroyedByExplosionFunction(w), world, (w) -> {
+            ServerCommandSource source = w.getServer().getCommandSource().withPosition(Vec3d.of(pos));
+            if (explosion.getCausingEntity() != null) {
+                source.withEntity(explosion.getCausingEntity());
+            }
+            return source;
+        }, (w) -> {
+            super.onDestroyedByExplosion(world, pos, explosion);
+        });
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        executeFunction((w) -> this.type.getStepOnFunction(w), world, (w) -> w.getServer().getCommandSource().withPosition(Vec3d.of(pos)).withEntity(entity), (w) -> {
+            super.onSteppedOn(world, pos, state, entity);
+        });
+    }
+
+    @Override
+    public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
+        executeFunction((w) -> this.type.getOnDestroyFunction(w), world, (w) -> w.getServer().getCommandSource().withPosition(Vec3d.of(pos)).withEntity(player), (w) -> {
+            super.afterBreak(world, player, pos, state, blockEntity, stack);
+        });
+    }
+}
